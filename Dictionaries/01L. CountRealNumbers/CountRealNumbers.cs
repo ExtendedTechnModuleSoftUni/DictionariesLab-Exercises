af8177e42bab2fc12accef282945242d27e1754d
VenciSarov <venci.sarov@gmail.com>
2017-03-01 07:36:04 +0200

    Adding two solutions from Dictionaries LAB

---
+++ Dictionaries/01L. CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,33 @@
+﻿namespace _01L.CountRealNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class CountRealNumbers
+    {
+        static void Main()
+        {
+            var realNumbersInput = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            var counts = new SortedDictionary<double, int>();
+
+            foreach (var num in realNumbersInput)
+            {
+                if (!counts.ContainsKey(num))
+                {
+                    counts[num] = 0;
+                }
+
+                counts[num]++;
+            }
+
+            foreach (var kvp in counts)
+            {
+                //string timeString = kvp.Value == 1 ? "time" : "times";
+
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
